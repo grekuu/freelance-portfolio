aa0e7ed627bb1f584dafa02cb042a5553bec9d8f
Gracjan <g.prusik@nextrope.com>
2025-05-09 16:52:00 +0200

    progress save

--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -1,21 +1,37 @@
-import Image from "next/image";
-import styles from "./Header.module.scss";
-import Link from "next/link";
+import Image from 'next/image';
+import styles from './Header.module.scss';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <Link className={styles.headerLogo} href="/">
-        <Image
-          src="/logo.webp"
-          alt="logo"
-          width={562}
-          height={239}
-          className={styles.logo}
-        />
-      </Link>
-      <div className={styles.linksContainer}></div>
-      <div className={styles.contactBtn}></div>
+      <div className={styles.contentContainer}>
+        <div className={styles.linksContainer}>
+          <Link href="#works">Works</Link>
+          <Link href="#testimonials">Testimonials</Link>
+          <Link href="#services">Services</Link>
+          <Link href="#about">About</Link>
+        </div>
+        <Link className={styles.headerLogo} href="/">
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={562}
+            height={239}
+            className={styles.logo}
+          />
+        </Link>
+        <Link className={styles.contactBtn} href="#contact">
+          <Image
+            src="/profilePic.webp"
+            width={32}
+            height={32}
+            alt="Contact Me Profilr Picture"
+            className={styles.profilePic}
+          />
+          Contact me
+        </Link>
+      </div>
     </header>
   );
 };
--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,5 +1,14 @@
+import styles from './Hero.module.scss';
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className={styles.container}>
+      <div>Ready to launch your online presence?</div>
+      Your website, fully designed, built, and launched — with a seamless
+      process that saves you time, eliminates stress, and gets you online
+      faster.
+    </section>
+  );
 };
 
 export default Hero;
