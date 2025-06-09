794efe7281d0e0b5bb178fd686787fae1fe3843e
unknown <g.prusik@nextrope.com>
2025-06-09 21:27:47 +0200

    feat: 🎸 init process container

--- src/sections/Process/Process.tsx
+++ src/sections/Process/Process.tsx
@@ -1,7 +1,16 @@
+import { ContentContainer, DecorativeLines, SectionTag } from '@/components';
 import styles from './Process.module.scss';
 
 const Process = () => {
-  return <section className={styles.container}>Process</section>;
+  return (
+    <section className={styles.container}>
+      <DecorativeLines count={2} darkSection />
+      <ContentContainer>
+        <SectionTag text="Process" darkSection />
+        <h3 className={styles.sectionTitle}>Simple steps</h3>
+      </ContentContainer>
+    </section>
+  );
 };
 
 export default Process;
