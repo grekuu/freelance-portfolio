e0ec9000de19cd2d5023571e0a0b42317091738f
Gracjan <g.prusik@nextrope.com>
2025-06-13 12:48:21 +0200

    feat: 🎸 process desktop + mobile

--- src/sections/Process/Process.tsx
+++ src/sections/Process/Process.tsx
@@ -24,6 +24,23 @@ const Process = () => {
 
     gsap.set([text1, text2, text3], { opacity: 0 });
 
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: container,
+        start: 'top top',
+        end: '+=2500',
+        pin: true,
+        scrub: 0.5,
+      },
+    });
+
+    tl.to(text1, { opacity: 1, duration: 0.1 })
+      .to(text1, { opacity: 0, duration: 0.1 }, 0.23)
+      .to(text2, { opacity: 1, duration: 0.1 }, 0.33)
+      .to(text2, { opacity: 0, duration: 0.1 }, 0.56)
+      .to(text3, { opacity: 1, duration: 0.1 }, 0.66)
+      .to(text3, { opacity: 0, duration: 0.1 }, 0.9);
+
     return () => {
       ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
     };
@@ -37,7 +54,7 @@ const Process = () => {
         <h3 className={styles.sectionTitle}>Simple steps</h3>
 
         <div ref={textRef1} className={styles.stepText}>
-          <h2>01</h2>
+          <span>01</span>
           <p>
             We’ll start with a quick call to understand your business, goals,
             and challenges — so the solution fits exactly what you need.
@@ -45,7 +62,7 @@ const Process = () => {
         </div>
 
         <div ref={textRef2} className={styles.stepText}>
-          <h2>02</h2>
+          <span>02</span>
           <p>
             You’ll get a clear plan with scope, price, and timeline tailored to
             your stage.
@@ -53,7 +70,7 @@ const Process = () => {
         </div>
 
         <div ref={textRef3} className={styles.stepText}>
-          <h2>03</h2>
+          <span>03</span>
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos,
             laudantium?
